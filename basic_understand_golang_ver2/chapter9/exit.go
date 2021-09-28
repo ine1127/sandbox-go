9d21f897a6e052609a68bcd3a9e1e89ad268ccb2
ine1127 <st_bluesunburst@yahoo.co.jp>
2021-09-29 00:11:57 +0900

    exit.go

---
+++ basic_understand_golang_ver2/chapter9/exit.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "log"
+  "os"
+)
+
+func main() {
+  _, err := os.OpenFile("test", os.O_RDONLY, 0)
+
+  if err != nil {
+    log.Println("エラー", err)
+
+    os.Exit(1)
+  }
+}
