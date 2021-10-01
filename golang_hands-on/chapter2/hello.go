0395a79dd2a4897852a82a1367e7b99fbf735312
ine1127 <st_bluesunburst@yahoo.co.jp>
2021-10-02 00:17:44 +0900

    list2-6

--- golang_hands-on/chapter2/hello.go
+++ golang_hands-on/chapter2/hello.go
@@ -8,15 +8,21 @@ import (
 
 func main() {
   x := hello.Input("type a price")
-  fmt.Print(x + "は、")
+  fmt.Print(x + "月は、")
 
-  if n, err := strconv.Atoi(x); err == nil {
-    if n % 2 == 0 {
-      fmt.Println("偶数です。")
-    } else {
-      fmt.Println("奇数です。")
-    }
-  } else {
-    fmt.Println("整数ではありません。")
+  switch n, err := strconv.Atoi(x); n {
+  case 0:
+    fmt.Println("整数値が得られません。")
+    fmt.Println(err)
+  case 1, 2, 12:
+    fmt.Println("冬です。")
+  case 3, 4, 5:
+    fmt.Println("春です。")
+  case 6, 7, 8:
+    fmt.Println("夏です。")
+  case 9, 10, 11:
+    fmt.Println("秋です。")
+  default:
+    fmt.Println("月の値ではありませんよ？")
   }
 }
