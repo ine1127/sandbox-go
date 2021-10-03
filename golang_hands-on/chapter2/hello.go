820188773d14936032ca3d28fe679bd8f91bb862
ine1127 <st_bluesunburst@yahoo.co.jp>
2021-10-04 00:25:27 +0900

    list2-10

--- golang_hands-on/chapter2/hello.go
+++ golang_hands-on/chapter2/hello.go
@@ -7,33 +7,21 @@ import (
 )
 
 func main() {
-  x := hello.Input("type 1~5")
+  x := hello.Input("type a number")
   n, err := strconv.Atoi(x)
 
   if err == nil {
-    fmt.Print(x + "までの合計は、")
+    fmt.Print("1から" + x + "の合計は、")
   } else {
     return
   }
   t := 0
-  switch n {
-  case 5:
-    t += 5
-    fallthrough
-  case 4:
-    t += 4
-    fallthrough
-  case 3:
-    t += 3
-    fallthrough
-  case 2:
-    t += 2
-    fallthrough
-  case 1:
-    t++
-  default:
-    fmt.Println("範囲外です。")
-    return
+  c := 1
+
+  for c <= n {
+    t += c
+    c++
   }
+
   fmt.Println(t, "です。")
 }
