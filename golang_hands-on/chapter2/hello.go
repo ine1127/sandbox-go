21c4cb682c97dc0f8fc05e869fe57578eae1c16d
ine1127 <st_bluesunburst@yahoo.co.jp>
2021-10-03 14:21:37 +0900

    list2-7

--- golang_hands-on/chapter2/hello.go
+++ golang_hands-on/chapter2/hello.go
@@ -2,27 +2,25 @@ package main
 
 import (
   "fmt"
-  "hello/hello"
-  "strconv"
+//  "hello/hello"
+//  "strconv"
 )
 
 func main() {
-  x := hello.Input("type a price")
-  fmt.Print(x + "月は、")
-
-  switch n, err := strconv.Atoi(x); n {
-  case 0:
-    fmt.Println("整数値が得られません。")
-    fmt.Println(err)
-  case 1, 2, 12:
-    fmt.Println("冬です。")
-  case 3, 4, 5:
-    fmt.Println("春です。")
-  case 6, 7, 8:
-    fmt.Println("夏です。")
-  case 9, 10, 11:
-    fmt.Println("秋です。")
+  x := 5
+  switch x {
+  case f(1):
+    fmt.Println("* first case. *")
+  case f(2):
+    fmt.Println("* sencond case. *")
+  case f(3):
+    fmt.Println("* third case. *")
   default:
-    fmt.Println("月の値ではありませんよ？")
+    fmt.Println("* default case. *")
   }
+}
+
+func f(n int) int {
+  fmt.Println("No,", n)
+  return n
 }
