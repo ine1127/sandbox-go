1077205d3063faa88f092cf888dcff8ea8b37944
ine1127 <st_bluesunburst@yahoo.co.jp>
2021-10-03 14:28:55 +0900

    list2-9

--- golang_hands-on/chapter2/hello.go
+++ golang_hands-on/chapter2/hello.go
@@ -7,19 +7,33 @@ import (
 )
 
 func main() {
-  x := hello.Input("type a number")
+  x := hello.Input("type 1~5")
   n, err := strconv.Atoi(x)
 
   if err == nil {
-    fmt.Print(x + "は、")
+    fmt.Print(x + "までの合計は、")
   } else {
     return
   }
-
-  switch {
-  case n % 2 == 0:
-    fmt.Println("偶数です。")
-  case n % 2 == 1:
-    fmt.Println("奇数です。")
+  t := 0
+  switch n {
+  case 5:
+    t += 5
+    fallthrough
+  case 4:
+    t += 4
+    fallthrough
+  case 3:
+    t += 3
+    fallthrough
+  case 2:
+    t += 2
+    fallthrough
+  case 1:
+    t++
+  default:
+    fmt.Println("範囲外です。")
+    return
   }
+  fmt.Println(t, "です。")
 }
