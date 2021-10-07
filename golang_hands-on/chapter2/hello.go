be66143ba24c1d19c990549bfa5f400f786a7006
ine1127 <st_bluesunburst@yahoo.co.jp>
2021-10-08 00:38:25 +0900

    list2-19

--- golang_hands-on/chapter2/hello.go
+++ golang_hands-on/chapter2/hello.go
@@ -8,16 +8,51 @@ import (
 )
 
 func main() {
-  a := [3]int{10, 20, 30}
-  b := a[0:2]
+  a := []int{10, 20, 30}
   fmt.Println(a)
-  fmt.Println(b)
 
-  b = append(b, 1000)
+  a = push(a, 1000)
   fmt.Println(a)
-  fmt.Println(b)
 
-  b = append(b, 1000)
+  a = pop(a)
   fmt.Println(a)
-  fmt.Println(b)
+
+  a = unshift(a, 1000)
+  fmt.Println(a)
+
+  a = shift(a)
+  fmt.Println(a)
+
+  a = insert(a, 1000, 2)
+  fmt.Println(a)
+
+  a = remove(a, 2)
+  fmt.Println(a)
+}
+
+func push(a []int, v int) []int {
+  return append(a, v)
+}
+
+func pop(a []int) []int {
+  return a[:len(a)-1]
+}
+
+func unshift(a []int, v int) []int {
+  return append([]int{v}, a...)
+}
+
+func shift(a []int) []int {
+  return a[1:]
+}
+
+func insert(a []int, v int, p int) []int {
+  a = append(a, 0)
+  a = append(a[:p+1], a[p:len(a)-1]...)
+  a[p] = v
+  return a
+}
+
+func remove(a []int, p int) []int {
+  return append(a[:p], a[p+1:]...)
 }
