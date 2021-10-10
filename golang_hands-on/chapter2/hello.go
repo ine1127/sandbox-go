38bca2593fdfcf3e7f25422bdf3c81440848f277
ine1127 <st_bluesunburst@yahoo.co.jp>
2021-10-10 21:14:00 +0900

    list2-26

--- golang_hands-on/chapter2/hello.go
+++ golang_hands-on/chapter2/hello.go
@@ -13,14 +13,11 @@ func main() {
   }
 
   fmt.Println(m)
-  m = insert(m, "*", 2)
-  m = insert(m, "*", 1)
+  m = push(m, "1", "2", "3")
   fmt.Println(m)
 }
 
-func insert(a []string, v string, p int) (s []string) {
-  s = append(a, "")
-  s = append(s[:p+1], s[p:len(s)-1]...)
-  s[p] = v
+func push(a []string, v ...string) (s []string) {
+  s = append(a, v...)
   return
 }
