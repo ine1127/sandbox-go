32af05db4f843559027f342c8d38b5652ed68d6c
ine1127 <st_bluesunburst@yahoo.co.jp>
2021-10-12 01:20:26 +0900

    list2-30

--- golang_hands-on/chapter2/hello.go
+++ golang_hands-on/chapter2/hello.go
@@ -2,27 +2,11 @@ package main
 
 import (
   "fmt"
-  _ "hello/hello"
-  _ "strconv"
-  _ "strings"
 )
 
 func main() {
-  data := "*新しい値"
-  m1 := modify(data)
-  data = "+new data+"
-  m2 := modify(data)
-
-  fmt.Println(m1())
-  fmt.Println(m2())
-}
-
-func modify(d string) func() []string {
-  m := []string {
-    "1st", "2nd",
-  }
-
-  return func() []string {
-    return append(m, d)
-  }
+  n := 123
+  b := true
+  s := "hello"
+  fmt.Printf("number: %d, bool: %t, string: %s.\n", n, b, s)
 }
