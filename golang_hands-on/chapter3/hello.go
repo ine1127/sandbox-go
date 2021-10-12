ba6136622cbd2a22bc7ca6d171d20db9cf878b1b
ine1127 <st_bluesunburst@yahoo.co.jp>
2021-10-13 00:19:38 +0900

    list3-2

--- golang_hands-on/chapter3/hello.go
+++ golang_hands-on/chapter3/hello.go
@@ -7,8 +7,14 @@ import (
 func main () {
   n := 123
   p := &n
+  m := 10000
+  p2 := &m
+  fmt.Printf("p  value:%d, address:%p\n", *p, p)
+  fmt.Printf("p2 value:%d, address:%p\n", *p2, p2)
 
-  fmt.Println("number:", n)
-  fmt.Println("pointer:", p)
-  fmt.Println("value:", *p)
+  pb := p
+  p = p2
+  p2 = pb
+  fmt.Printf("p  value:%d, address:%p\n", *p, p)
+  fmt.Printf("p2 value:%d, address:%p\n", *p2, p2)
 }
