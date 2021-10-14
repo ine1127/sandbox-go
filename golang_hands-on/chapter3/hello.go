a6cfb05920ceac4fd3feced3daf0659262c31afc
ine1127 <st_bluesunburst@yahoo.co.jp>
2021-10-15 00:58:50 +0900

    list3-7

--- golang_hands-on/chapter3/hello.go
+++ golang_hands-on/chapter3/hello.go
@@ -4,13 +4,19 @@ import (
   "fmt"
 )
 
-var mydata struct {
+// Mydata is structure
+type Mydata struct {
   Name string
   Data []int
 }
 
 func main () {
-  mydata.Name = "Taro"
-  mydata.Data = []int{10, 20, 30}
-  fmt.Println(mydata)
+  taro := Mydata{"Taro", []int{10, 20, 30},}
+  hanako := Mydata{
+          Name: "Hanako",
+          Data: []int{90, 80, 70},
+  }
+
+  fmt.Println(taro)
+  fmt.Println(hanako)
 }
