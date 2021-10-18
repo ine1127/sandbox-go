4c827f5e23415c7cdc35bbafa5e5b67bf99b477c
ine1127 <st_bluesunburst@yahoo.co.jp>
2021-10-18 23:17:17 +0900

    list3-14

--- golang_hands-on/chapter3/hello.go
+++ golang_hands-on/chapter3/hello.go
@@ -2,61 +2,34 @@ package main
 
 import (
   "fmt"
-  "strconv"
-  "hello/hello"
 )
 
-type intp int
-
-func (num intp) IsPrime() bool {
-  n := int(num)
-  for i := 2; i <= (n / 2); i++ {
-    if n % i == 0 {
-      return false
-    }
-  }
-  return true
+// Data is interface
+type Data interface {
+  Initial(name string, data []int)
+  PrintData()
 }
 
-func (num intp) PrimeFactor() []int {
-  ar := []int{}
-  x := int(num)
-  n := 2
-
-  for x > n {
-    if x % n == 0 {
-      x /= n
-      ar = append(ar, n)
-    } else {
-      if n == 2 {
-        n++
-      } else {
-        n+= 2
-      }
-    }
-  }
-
-  ar = append(ar, x)
-  return ar
+// Mydata is Struct.
+type Mydata struct {
+  Name string
+  Data []int
 }
 
-func (num *intp) doPrime() {
-  pf := num.PrimeFactor()
-  *num = intp(pf[len(pf)-1])
+// Initial is init method
+func (md *Mydata) Initial(name string, data []int) {
+  md.Name = name
+  md.Data = data
 }
 
-func main () {
-  s := hello.Input("type a number")
-  n, _ := strconv.Atoi(s)
-  x := intp(n)
-  fmt.Printf("%d [%t].\n", x, x.IsPrime())
-  fmt.Println(x.PrimeFactor())
-  x.doPrime()
+// PrintData is println all data.
+func (md *Mydata) PrintData() {
+  fmt.Println("Name: ", md.Name)
+  fmt.Println("Data: ", md.Data)
+}
 
-  fmt.Printf("%d [%t].\n", x, x.IsPrime())
-  fmt.Println(x.PrimeFactor())
-  x++
-
-  fmt.Printf("%d [%t].\n", x, x.IsPrime())
-  fmt.Println(x.PrimeFactor())
+func main() {
+  var ob Mydata = Mydata{}
+  ob.Initial("Sachiko", []int{55, 66, 77})
+  ob.PrintData()
 }
