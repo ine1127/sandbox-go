a53991d9b87ab5c466abff038faecd4158f86bfb
ine1127 <st_bluesunburst@yahoo.co.jp>
2021-10-20 01:03:52 +0900

    list3-18

--- golang_hands-on/chapter3/hello.go
+++ golang_hands-on/chapter3/hello.go
@@ -2,40 +2,79 @@ package main
 
 import (
   "fmt"
+  "strings"
+  "strconv"
 )
 
 // Data is interface
 type Data interface {
-  Initial(name string, data []int)
+  SetValue(vals map[string]string)
   PrintData()
-  Check()
 }
 
-// Mydata is Struct.
+// Mydata is structure.
 type Mydata struct {
   Name string
   Data []int
 }
 
-// Initial is init method
-func (md *Mydata) Initial(name string, data []int) {
-  md.Name = name
-  md.Data = data
+// SetValue is Mydata method.
+func (md *Mydata) SetValue(vals map[string]string) {
+  md.Name = vals["name"]
+  valt := strings.Split(vals["data"], " ")
+  vali := []int{}
+
+  for _, i := range valt {
+    n, _ := strconv.Atoi(i)
+    vali = append(vali, n)
+  }
+  md.Data = vali
 }
 
-// PrintData is println all data.
+// PrintData is Mydata method.
 func (md *Mydata) PrintData() {
   fmt.Println("Name: ", md.Name)
   fmt.Println("Data: ", md.Data)
 }
 
-// Check is method.
-func (md *Mydata) Check() {
-  fmt.Printf("Check! [%s]\n", md.Name)
+// Yourdata is structure.
+type Yourdata struct {
+  Name string
+  Mail string
+  Age int
+}
+
+// SetValue is Yourdata method.
+func (md *Yourdata) SetValue(vals map[string]string) {
+  md.Name = vals["name"]
+  md.Mail = vals["mail"]
+  n, _ := strconv.Atoi(vals["age"])
+  md.Age = n
+}
+
+// PrintData is Yourdata method.
+func (md *Yourdata) PrintData() {
+  fmt.Printf("I'm %s. (%d).\n", md.Name, md.Age)
+  fmt.Printf("mail: %s.\n", md.Mail)
 }
 
 func main() {
-  var ob Data = new(Mydata)
-  ob.Initial("Sachiko", []int{55, 66, 77})
-  ob.Check()
+  ob := [2]Data{}
+  ob[0] = new(Mydata)
+  ob[0].SetValue(map[string]string{
+          "name": "Sachiko",
+          "data": "55, 66, 77",
+  })
+
+  ob[1] = new(Yourdata)
+  ob[1].SetValue(map[string]string{
+          "name": "Mami",
+          "mail": "mami@mume.mo",
+          "age": "34",
+  })
+
+  for _, d := range ob {
+    d.PrintData()
+    fmt.Println()
+  }
 }
