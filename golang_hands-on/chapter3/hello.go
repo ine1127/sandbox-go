12b3e3e2f75391816db845fdf67ede789f605d1d
ine1127 <st_bluesunburst@yahoo.co.jp>
2022-06-15 00:05:26 +0900

    list3-26

--- golang_hands-on/chapter3/hello.go
+++ golang_hands-on/chapter3/hello.go
@@ -2,66 +2,17 @@ package main
 
 import (
   "fmt"
-  "reflect"
+  "time"
 )
 
-// General is all type data.
-type General interface{}
-
-// GData is holding General value.
-type GData interface {
-  Set(nm string, g General) GData
-  Print()
-}
-
-// Ndata is structure.
-type NData struct {
-  Name string
-  Data []int
-}
-
-// Set is NData method.
-func (nd *NData) Set(nm string, g General) GData {
-  nd.Name = nm
-  if reflect.TypeOf(g) == reflect.SliceOf(reflect.TypeOf(0)) {
-    nd.Data = g.([]int)
+func hello(s string, n int) {
+  for i := 1; i <= 10; i++ {
+    fmt.Printf("<%d %s>", i, s)
+    time.Sleep(time.Duration(n) * time.Millisecond)
   }
-  return nd
-}
-
-// Print is NData  method.
-func (nd *NData) Print() {
-  fmt.Printf("<<%s>> value: %d\n", nd.Name, nd.Data)
-}
-
-// SData is structure.
-type SData struct {
-  Name string
-  Data []string
-}
-
-// Set is SData method.
-func (sd *SData) Set(nm string, g General) GData {
-  sd.Name = nm
-  if reflect.TypeOf(g) == reflect.SliceOf(reflect.TypeOf("")) {
-    sd.Data = g.([]string)
-  }
-  return sd
-}
-
-// Print is SData method.
-func (sd *SData) Print() {
-  fmt.Printf("* %s [%s] *\n", sd.Name, sd.Data)
 }
 
 func main() {
-  var data = []GData{}
-  data = append(data, new(NData).Set("Taro", []int{1, 2, 3}))
-  data = append(data, new(SData).Set("Jiro", []string{"hello", "bye"}))
-  data = append(data, new(NData).Set("Hanako", 98700))
-  data = append(data, new(SData).Set("Sachiko", "happy?"))
-
-  for _, ob := range data {
-    ob.Print()
-  }
+  go hello("hello", 50)
+  hello("bye!", 100)
 }
