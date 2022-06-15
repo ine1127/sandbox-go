891302027ebde3b079d4801f382c764893bc7e8f
ine1127 <st_bluesunburst@yahoo.co.jp>
2022-06-15 23:49:05 +0900

    list3-27

--- golang_hands-on/chapter3/hello.go
+++ golang_hands-on/chapter3/hello.go
@@ -3,16 +3,31 @@ package main
 import (
   "fmt"
   "time"
+  "strconv"
 )
 
-func hello(s string, n int) {
-  for i := 1; i <= 10; i++ {
-    fmt.Printf("<%d %s>", i, s)
+func main() {
+  msg := "start"
+  prmsg := func(nm string, n int) {
+    fmt.Println(nm, msg)
     time.Sleep(time.Duration(n) * time.Millisecond)
   }
-}
 
-func main() {
-  go hello("hello", 50)
-  hello("bye!", 100)
+  hello := func(n int) {
+    const nm string = "hello"
+    for i := 0; i < 10; i++ {
+      msg += " h" + strconv.Itoa(i)
+      prmsg(nm, n)
+    }
+  }
+
+  main := func(n int) {
+    const nm string = "*main"
+    for i := 0; i < 5; i++ {
+      msg += " m" + strconv.Itoa(i)
+      prmsg(nm, 100)
+    }
+  }
+  go hello(60)
+  main(100)
 }
