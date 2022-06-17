f837f97e33e1f41ce4f905aae6355d14be34d541
ine1127 <st_bluesunburst@yahoo.co.jp>
2022-06-18 02:08:51 +0900

    list3-28

--- golang_hands-on/chapter3/hello.go
+++ golang_hands-on/chapter3/hello.go
@@ -2,32 +2,19 @@ package main
 
 import (
   "fmt"
-  "time"
-  "strconv"
+  // "time"
 )
 
+func total(n int, c chan int) {
+  t := 0
+  for i := 1; i <= n; i++ {
+    t += 1
+  }
+  c <- t
+}
+
 func main() {
-  msg := "start"
-  prmsg := func(nm string, n int) {
-    fmt.Println(nm, msg)
-    time.Sleep(time.Duration(n) * time.Millisecond)
-  }
-
-  hello := func(n int) {
-    const nm string = "hello"
-    for i := 0; i < 10; i++ {
-      msg += " h" + strconv.Itoa(i)
-      prmsg(nm, n)
-    }
-  }
-
-  main := func(n int) {
-    const nm string = "*main"
-    for i := 0; i < 5; i++ {
-      msg += " m" + strconv.Itoa(i)
-      prmsg(nm, 100)
-    }
-  }
-  go hello(60)
-  main(100)
+  c := make(chan int)
+  go total(100, c)
+  fmt.Println("total:", <-c)
 }
