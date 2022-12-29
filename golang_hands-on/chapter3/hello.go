79c8ccfbe1c83eca17c341c441fcaf3a0e2385a9
ine1127 <st_bluesunburst@yahoo.co.jp>
2022-12-29 17:25:43 +0900

    list3-31

--- golang_hands-on/chapter3/hello.go
+++ golang_hands-on/chapter3/hello.go
@@ -18,12 +18,8 @@ func total(c chan int) {
 
 func main() {
 	c := make(chan int)
-	// この時点でchanに値は設定できない。
-	// chanは複数スレッド間で値をやり取りするためのものであるが、
-	// 送信側、受信側の双方で値の準備が整っていないといけないため。
-	// このため、Goroutineによるスレッドを実行した後でないとchanは使えない
-  // なので、このプログラムは失敗する
-	c <- 100
+	// Goroutineを先に持ってくることでchanに値を設定することができる
 	go total(c)
+	c <- 100
 	time.Sleep(100 * time.Millisecond)
 }
