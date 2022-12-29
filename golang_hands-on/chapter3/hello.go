9a438413b5d4afca8be53211a4ed850548ef1b35
ine1127 <st_bluesunburst@yahoo.co.jp>
2022-12-29 16:59:21 +0900

    list3-29

--- golang_hands-on/chapter3/hello.go
+++ golang_hands-on/chapter3/hello.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-  "fmt"
-  // "time"
+	"fmt"
 )
 
 func total(n int, c chan int) {
-  t := 0
-  for i := 1; i <= n; i++ {
-    t += 1
-  }
-  c <- t
+	t := 0
+
+	for i := 1; i <= n; i++ {
+		t += i
+	}
+
+	c <- t
 }
 
 func main() {
-  c := make(chan int)
-  go total(100, c)
-  fmt.Println("total:", <-c)
+	c := make(chan int)
+	go total(1000, c)
+	go total(100, c)
+	go total(10, c)
+
+	x, y, z := <-c, <-c, <-c
+	fmt.Println(x, y, z)
 }
