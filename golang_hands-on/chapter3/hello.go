ff74da5690a218d8476d140d7920bebbb3ed1cbc
ine1127 <st_bluesunburst@yahoo.co.jp>
2023-01-18 15:24:29 +0900

    list3-33

--- golang_hands-on/chapter3/hello.go
+++ golang_hands-on/chapter3/hello.go
@@ -2,32 +2,24 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"time"
+	_ "strconv"
+	_ "time"
 )
 
-func prmsg(n int, s string) {
-	fmt.Println(s)
-	time.Sleep(time.Duration(n) * time.Millisecond)
-}
-
-func first(n int, c chan string) {
-	const nm string = "first-"
-	for i := 0; i < 10; i++ {
-		s := nm + strconv.Itoa(i)
-		prmsg(n, s)
-		c <- s
-	}
-}
-
-func second(n int, c chan string) {
-	for i := 0; i < 10; i++ {
-		prmsg(n, "second:["+<-c+"]")
-	}
+func total(cs chan int, cr chan int) {
+  n := <-cs
+  fmt.Println("n = ", n)
+  t := 0
+  for i := 1; i <= n; i++ {
+    t += i
+  }
+  cr <- t
 }
 
 func main() {
-	c := make(chan string)
-	go first(10, c)
-	second(10, c)
+	cs := make(chan int)
+  cr := make(chan int)
+	go total(cs, cr)
+  cs <- 100
+  fmt.Println("total:", <-cr)
 }
