1c008133091b6aef0aaa394c3fd9e55b1fad425d
ine1127 <st_bluesunburst@yahoo.co.jp>
2023-01-18 17:23:06 +0900

    list3-35

--- golang_hands-on/chapter3/hello.go
+++ golang_hands-on/chapter3/hello.go
@@ -2,38 +2,47 @@ package main
 
 import (
 	"fmt"
-	_ "strconv"
+	"strconv"
+  "sync"
 	"time"
 )
 
-func count(n int, s int, c chan int) {
-	for i := 1; i <= n; i++ {
-		c <- i
-		time.Sleep(time.Duration(s) * time.Millisecond)
-	}
+// SrData is structure.
+type SrData struct {
+  msg string
+  mux sync.Mutex
 }
 
 func main() {
-	n1, n2, n3 := 3, 5, 10
-	m1, m2, m3 := 100, 75, 50
-	c1 := make(chan int)
-	go count(n1, m1, c1)
+  sd := SrData{msg: "Start"}
+  prmsg := func(nm string, n int) {
+    fmt.Println(nm, sd.msg)
+    time.Sleep(time.Duration(n) * time.Millisecond)
+  }
 
-	c2 := make(chan int)
-	go count(n2, m2, c2)
+  main := func(n int) {
+    const nm string = "*main"
+    sd.mux.Lock() // lock
 
-	c3 := make(chan int)
-	go count(n3, m3, c3)
+    for i := 0; i < 5; i++ {
+      sd.msg += " m" + strconv.Itoa(i)
+      prmsg(nm, 100)
+    }
+    sd.mux.Unlock() // unlock
+  }
 
-	for i := 0; i < n1+n2+n3; i++ {
-		select {
-		case re := <-c1:
-			fmt.Println("*  first ", re)
-		case re := <-c2:
-			fmt.Println("** second", re)
-		case re := <-c3:
-			fmt.Println("***third ", re)
-		}
-	}
-	fmt.Println("*** finish. ***")
+  hello := func(n int) {
+    const nm string = "hello"
+    sd.mux.Lock() // lock
+
+    for i := 0; i < 5; i++ {
+      sd.msg += " h" + strconv.Itoa(i)
+      prmsg(nm, n)
+    }
+    sd.mux.Unlock() // unlock
+  }
+
+  go main(100)
+  go hello(50)
+  time.Sleep(5 * time.Second)
 }
