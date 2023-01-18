7baa5b6e6d7f5c3abc169082f9bec8d5183fd219
ine1127 <st_bluesunburst@yahoo.co.jp>
2023-01-18 16:34:23 +0900

    list3-34

--- golang_hands-on/chapter3/hello.go
+++ golang_hands-on/chapter3/hello.go
@@ -3,23 +3,37 @@ package main
 import (
 	"fmt"
 	_ "strconv"
-	_ "time"
+	"time"
 )
 
-func total(cs chan int, cr chan int) {
-  n := <-cs
-  fmt.Println("n = ", n)
-  t := 0
-  for i := 1; i <= n; i++ {
-    t += i
-  }
-  cr <- t
+func count(n int, s int, c chan int) {
+	for i := 1; i <= n; i++ {
+		c <- i
+		time.Sleep(time.Duration(s) * time.Millisecond)
+	}
 }
 
 func main() {
-	cs := make(chan int)
-  cr := make(chan int)
-	go total(cs, cr)
-  cs <- 100
-  fmt.Println("total:", <-cr)
+	n1, n2, n3 := 3, 5, 10
+	m1, m2, m3 := 100, 75, 50
+	c1 := make(chan int)
+	go count(n1, m1, c1)
+
+	c2 := make(chan int)
+	go count(n2, m2, c2)
+
+	c3 := make(chan int)
+	go count(n3, m3, c3)
+
+	for i := 0; i < n1+n2+n3; i++ {
+		select {
+		case re := <-c1:
+			fmt.Println("*  first ", re)
+		case re := <-c2:
+			fmt.Println("** second", re)
+		case re := <-c3:
+			fmt.Println("***third ", re)
+		}
+	}
+	fmt.Println("*** finish. ***")
 }
